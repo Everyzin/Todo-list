0522b46026e0771761d10c2af34fcccaebd067c4
Everyzin <142842132+Everyzin@users.noreply.github.com>
2023-09-11 11:35:28 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,67 @@
 module.exports = {
+
   env: {
+
     node: true,
+
     commonjs: true,
+
     browser: true,
+
     es6: true,
+
     jest: true,
+
   },
-  extends: ['airbnb', 'prettier'],
+
+  extends: ["airbnb", "prettier"],
+
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+
+    Atomics: "readonly",
+
+    SharedArrayBuffer: "readonly",
+
   },
-  parser: 'babel-eslint',
+
   parserOptions: {
+
     ecmaFeatures: {
+
       jsx: true,
+
     },
+
     ecmaVersion: 2018,
-    sourceType: 'module',
+
+    sourceType: "module",
+
   },
-  plugins: ['react', 'react-hooks', 'testing-library', 'prettier'],
+
+  plugins: ["react", "react-hooks", "testing-library", "prettier"],
+
   rules: {
-    'prettier/prettier': 'warn',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
+
+    "prettier/prettier": "warn",
+
+    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
+
+    "import/extensions": "off",
+
+    "import/prefer-default-export": "off",
+
+    "import/no-extraneous-dependencies": [
+
+      "error",
+
       {
-        packageDir: './',
-        devDependencies: true,
+
+        packageDir: "./",
+
       },
+
     ],
+
   },
-}
+
+};
